921f7a75d575391a3d5167bded1ebc1f9b44b077
Gal Ben Ami <benami171@gmail.com>
2024-09-16 12:19:23 +0300

    Initial commit

---
+++ Graph.hpp
@@ -0,0 +1,22 @@
+#ifndef OS_EXE4_GRAPH_HPP
+#define OS_EXE4_GRAPH_HPP
+
+#include <vector>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Graph {
+public:
+    vector<list<int>> adjList;
+    int vertices;
+    int edges;
+
+
+
+};
+
+
+
+#endif //OS_EXE4_GRAPH_HPP
