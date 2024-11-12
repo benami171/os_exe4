708424007bdcda795b4ba6fb13fe8b34ca921220
Gal Ben Ami <benami171@gmail.com>
2024-11-12 21:56:16 +0200

    looks like finding eulerian cycle works.

--- Graph.cpp
+++ Graph.cpp
@@ -100,10 +100,15 @@ void Graph::eulerFinder() {
 }
 
 int main() {
-    Graph g(5);
+    Graph g(7);
     g.addEdge(0, 1);
-    g.addEdge(1, 2);
-//    g.addEdge(2, 0);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+    g.addEdge(4, 5);
+    g.addEdge(4, 6);
+    g.addEdge(5, 6);
 
     g.eulerFinder();
     return 0;
