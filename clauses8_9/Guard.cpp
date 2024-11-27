658a04630b1c78070bacca69ada79998cec8f560
BarakRozenkvit <barak10101998@gmail.com>
2024-11-27 15:02:54 +0200

    add clauses 8 and 9 and Guard class

---
+++ clauses8_9/Guard.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Barak Rozenkvit on 27/11/2024.
+//
+
+#include "Guard.hpp"
---
+++ clauses8_9/Guard.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Barak Rozenkvit on 27/11/2024.
+//
+
+#ifndef OS_EXE4_GUARD_HPP
+#define OS_EXE4_GUARD_HPP
+
+
+class Guard {
+
+};
+
+
+#endif //OS_EXE4_GUARD_HPP
